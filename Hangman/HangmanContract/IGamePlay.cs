90435f3263b4c1ae9b6d2ada0fa5ac4153aca5c4
Stéfano <bistecapf@gmail.com>
2013-11-28 23:12:41 +0100

    created new solution
    including the 3 projects, and added the classes/interfaces

---
+++ Hangman/HangmanContract/IGamePlay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace HangmanContract
+{
+    [ServiceContract(Namespace = "HangmanContract")]
+    interface IGamePlay
+    {
+        [OperationContract]
+        void chooseGameWord(string gameWord, string username);
+
+        [OperationContract]
+        void guessLetter(char letter, string username);
+
+        [OperationContract]
+        void guessWord(string word, string username);
+
+        [OperationContract]
+        void timeUp(string username);
+
+        [OperationContract]
+        void chat(string message, string username);
+
+        [OperationContract]
+        void leaveGame(string username);
+    }
+}
---
+++ Hangman/HangmanContract/IPortal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace HangmanContract
+{
+    [ServiceContract(Namespace = "HangmanContract")]
+    interface IPortal
+    {
+        [OperationContract]
+        bool login(string username, string password);
+        
+        [OperationContract]
+        bool register(string username, string password);
+        
+        [OperationContract]
+        void invitePlayers(string[] invitedPlayerNames, string username); //invites players to a game
+        
+        [OperationContract]
+        void acceptInvitation(bool userAccepted);
+    }
+}
